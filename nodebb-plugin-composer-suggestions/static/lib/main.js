0bfabf3c66a3278f215d3432824ad6b59d336ad8
Dcano-png <dcano@andrew.cmu.edu>
2025-09-26 14:56:08 -0400

    main.js beginning UI implementation: added static suggested topics container for plugin-composer-suggestions

--- nodebb-plugin-composer-suggestions/static/lib/main.js
+++ nodebb-plugin-composer-suggestions/static/lib/main.js
@@ -3,10 +3,24 @@
 define('composer-suggestions/main', [], function () {
     console.log('[composer-suggestions] main.js entered');
     const Suggestions = {};
+    // ChatGPT Assisted Code Below (.addPlaceholder function)
+    Suggestions.addPlaceholder = function (data) {
+        console.log('[composer-suggestions] adding placeholder...');
 
-    Suggestions.init = function () {
-        console.log('[composer-suggestions] main.js loaded');
+        const composerEl = $('.composer[data-uuid="' + data.post_uuid + '"]');
+        const titleEl = composerEl.find('input.title');
+
+        if (titleEl.length && !composerEl.find('.suggested-topics').length) {
+            titleEl.after(
+                '<div class="suggested-topics"><strong>Suggested Topics will appear here...</strong></div>'
+            );
+        }
     };
+
+    // Run when composer loads
+    $(window).on('action:composer.loaded', function (ev, data) {
+        Suggestions.addPlaceholder(data);
+    });
 
     return Suggestions;
 });
