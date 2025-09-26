b9f7116ba28a7778b0799cbd21fd5aabf232f9fe
Dcano-png <dcano@andrew.cmu.edu>
2025-09-26 14:02:56 -0400

    main.js set up for plugin-composer-suggestions

---
+++ nodebb-plugin-composer-suggestions/static/lib/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+define('composer-suggestions/main', [], function () {
+    const Suggestions = {};
+
+    Suggestions.init = function () {
+        console.log('[composer-suggestions] main.js loaded');
+    };
+
+    return Suggestions;
+});
