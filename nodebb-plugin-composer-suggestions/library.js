2bbbf25c865814d535522c89c10fb7c4f9fd2ffa
Dcano-png <dcano@andrew.cmu.edu>
2025-10-08 15:18:15 -0400

    Fix library.js call on plug-in: Ensure console log shows

--- nodebb-plugin-composer-suggestions/library.js
+++ nodebb-plugin-composer-suggestions/library.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const plugin = {};
+const db = require.main.require('./src/database');
+const topics = require.main.require('./src/topics');
 
 plugin.init = async function (params) {
-    console.log('[composer-suggestions] init called');
+    const { router, middleware } = params;
+    console.log('[composer-suggestions] LIBRARY ACTIVE called');
+
+    
 };
 
 module.exports = plugin;
