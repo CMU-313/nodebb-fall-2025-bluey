56e9f68c6a43c4c67b1c718bb7ba99b28f461a46
Lisa Huang <wenleh@andrew.cmu.edu>
2025-11-09 01:30:27 +0000

    added network for microservice and nodebb to communicate

--- src/translate/index.js
+++ src/translate/index.js
@@ -5,7 +5,7 @@
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-	const TRANSLATOR_API = process.env.TRANSLATOR_API || 'http://translator:5000';
+	const TRANSLATOR_API = process.env.TRANSLATOR_API || 'http://translatorllm:5000';
 	const response = await fetch(`${TRANSLATOR_API}/?content=${encodeURIComponent(postData.content)}`);
 	const data = await response.json();
 	return [data.is_english, data.translated_content];
